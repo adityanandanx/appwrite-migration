5208de1b6158c9397cf42cdcda1e840a63827742
Sanchit Bajaj <sanchit02@outlook.com>
2024-02-10 01:05:19 +0530

    feat: jsDoc added onto the main class

--- src/client/AppwriteMigrationClient.ts
+++ src/client/AppwriteMigrationClient.ts
@@ -38,15 +38,39 @@ export default class AppwriteMigrationClient {
     return this.client;
   }
 
+  /**
+   * @abstract This method collects the data from the provided Appwrite account instance and returns the json to store it into the file.
+   * @param client {AppwriteMigrationClient}
+   * @returns json data
+   */
   public generateSchema() {
     return generateSchema(this);
   }
+
+  /**
+   * @abstract This method prepares the schema of collection and bucket into the appwrite instance that is provided. It accepts parsed json as input that is generated
+   * @param client
+   * @param schema
+   */
   public prepareSchema(schema: AppwriteSchema) {
     return prepareSchema(this, schema);
   }
+
+  /**
+   * @abstract This method method collects the data from the provided schema file which is generated using `generateSchema()` method.
+   * @param client
+   * @param schema
+   * @returns json data
+   */
   public generateData(schema: AppwriteSchema) {
     return generateData(this, schema);
   }
+
+  /**
+   * @abstract This method prepares the data of collection and bucket from .json file to migrate to the server. It accepts parsed json as input that is generated
+   * @param client
+   * @param data
+   */
   public prepareData(data: AppwriteData) {
     return prepareData(this, data);
   }
